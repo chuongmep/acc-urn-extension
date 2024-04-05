0c6ba1288e7e9569873b3bed5cf2e339179eed7f
Chuong Ho <chuongpqvn@gmail.com>
2024-04-05 15:00:33 +0800

    update  infor

--- popup.js
+++ popup.js
@@ -7,35 +7,26 @@ function fetchCurrentTabInfo() {
     chrome.tabs.query({
         active: true,
         currentWindow: true
-      }, ([currentTab]) => {
+    }, ([currentTab]) => {
         console.log("Current URL: " + currentTab.url);
-        if(isBIM360Url(currentTab.url)){
+        if (isBIM360Url(currentTab.url)) {
             fetchBIM360Url(currentTab.url);
         }
-        if(isAccUrl(currentTab.url))
-        {
-        fetchInfo(currentTab.url);
+        if (isAccUrl(currentTab.url)) {
+            fetchInfo(currentTab.url);
         }
     });
 }
 
 function fetchInfo(url) {
 
-    var project_id = url.match(/projects\/([^\/?#]+)/)?.[1] || '';
-    var urlParams = new URLSearchParams(url.split('?')[1] || '');
-    var folder_urn = urlParams.get('folderUrn') || '';
-    var entity_id = urlParams.get('entityId') || '';
-    var viewable_guid = urlParams.get('viewableGuid') || '';
+    let project_id = url.match(/projects\/([^\/?#]+)/)?.[1] || '';
+    let urlParams = new URLSearchParams(url.split('?')[1] || '');
+    let folder_urn = urlParams.get('folderUrn') || '';
+    let entity_id = urlParams.get('entityId') || '';
+    let viewable_guid = urlParams.get('viewableGuid') || '';
 
-    var output = "<div class='info-item'><label>Project ID:</label> " + project_id + "<button class='copy-button' data-value='" + project_id + "'>Copy</button></div>";
-    output += "<div class='info-item'><label>Folder URN:</label> " + folder_urn + "<button class='copy-button' data-value='" + folder_urn + "'>Copy</button></div>";
-    output += "<div class='info-item'><label>Entity ID:</label> " + entity_id + "<button class='copy-button' data-value='" + entity_id + "'>Copy</button></div>";
-    output += "<div class='info-item'><label>Viewable GUID:</label> " + viewable_guid + "<button class='copy-button' data-value='" + viewable_guid + "'>Copy</button></div>";
-
-
-    document.getElementById("infoOutput").innerHTML = output;
-    infoOutput.innerHTML = output;
-    infoOutput.style.display = "block";
+    generateOutput(project_id, folder_urn, entity_id, viewable_guid)
 
     document.querySelectorAll('.copy-button').forEach(button => {
         button.addEventListener('click', function () {
@@ -43,20 +34,25 @@ function fetchInfo(url) {
         });
     });
 }
-function fetchBIM360Url(){
-    var url = "https://docs.b360.eu.autodesk.com/projects/2f20ccbf-ae8e-4009-95c4-d2b146168053/folders/urn:adsk.wipemea:fs.folder:co.g3jcPQptRia-wjxxaBaWbQ/detail/viewer/items/urn:adsk.wipemea:dm.lineage:kr3w5Tz1R2u1kTomNf_Brw"
-    var project_id = url.match(/projects\/([^\/?#]+)/)?.[1] || '';
-    var folder_urn = url.match(/folders\/([^\/?#]+)/)?.[1] || '';
-    var entity_id = url.match(/items\/([^\/?#]+)/)?.[1] || '';
-    var viewable_guid = ""
-    
-    var output = "<div class='info-item'><label>Project ID:</label> " + project_id + "<button class='copy-button' data-value='" + project_id + "'>Copy</button></div>";
+
+function generateOutput(project_id, folder_urn, entity_id, viewable_guid) {
+    let output = "<div class='info-item'><label>Project ID:</label> " + project_id + "<button class='copy-button' data-value='" + project_id + "'>Copy</button></div>";
     output += "<div class='info-item'><label>Folder URN:</label> " + folder_urn + "<button class='copy-button' data-value='" + folder_urn + "'>Copy</button></div>";
     output += "<div class='info-item'><label>Entity ID:</label> " + entity_id + "<button class='copy-button' data-value='" + entity_id + "'>Copy</button></div>";
     output += "<div class='info-item'><label>Viewable GUID:</label> " + viewable_guid + "<button class='copy-button' data-value='" + viewable_guid + "'>Copy</button></div>";
     document.getElementById("infoOutput").innerHTML = output;
     infoOutput.innerHTML = output;
     infoOutput.style.display = "block";
+    return output;
+}
+
+function fetchBIM360Url(url) {
+    let project_id = url.match(/projects\/([^\/?#]+)/)?.[1] || '';
+    let folder_urn = url.match(/folders\/([^\/?#]+)/)?.[1] || '';
+    let entity_id = url.match(/items\/([^\/?#]+)/)?.[1] || '';
+    let viewable_guid = ""
+
+    generateOutput(project_id, folder_urn, entity_id, viewable_guid)
 
     document.querySelectorAll('.copy-button').forEach(button => {
         button.addEventListener('click', function () {
@@ -64,16 +60,18 @@ function fetchBIM360Url(){
         });
     });
 }
-function isBIM360Url(url){
+
+function isBIM360Url(url) {
     return url.includes("docs.b360");
 }
-function isAccUrl(url){
+
+function isAccUrl(url) {
     return url.includes("acc.autodesk.com");
 }
 
 
 function copyValue(value) {
-    var dummy = document.createElement("textarea");
+    let dummy = document.createElement("textarea");
     document.body.appendChild(dummy);
     dummy.value = value;
     dummy.select();
