a6667fcfc454d7b104d0bf1ab60eb930185a76bd
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2024-06-08 20:37:50 +0800

    add token

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 window.addEventListener("message", function(event) {
     if (event.data && event.data.type === "fetchUrn") {
         const urn = getUrn();  // Assuming getUrn is a function that gets the current URN
@@ -6,6 +7,28 @@ window.addEventListener("message", function(event) {
     }
 });
 
+window.addEventListener("message", function(event) {
+    if (event.data && event.data.type === "fetchToken") {
+        const token = getToken();  // Assuming getUrn is a function that gets the current URN
+        console.log("Fetching Token to send back to content script.");
+        window.postMessage({type: "sendToken", token: token}, "*");
+    }
+});
+
 function getUrn() {
-    return window.NOP_VIEWER.model.getData().urn;
+    if(window.NOP_VIEWER == null){
+        return "";
+    }
+    var result = window.NOP_VIEWER.model.getData().urn;
+    if(result!=null){
+        return result;
+    }
+    return "";
+}
+
+function getToken(){
+    if(window.Autodesk == null || window.Autodesk.Viewing == null || window.Autodesk.Viewing.token == null){
+        return "";
+    }
+   return window.Autodesk.Viewing.token.accessToken; 
 }
